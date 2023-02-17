a8f5944e5358019494faf5b2f93cd8de0a1a0813
zenit <zenit.03@yandex.ru>
2023-02-17 23:51:08 +0300

    2 3 4 5

---
+++ main.py
@@ -0,0 +1,72 @@
+import math
+
+
+def main1(x):
+    if x < 20:
+        return (29 * x) ** 7 - math.sin(x) ** 2
+    elif 20 <= x < 55:
+        return (x ** 2) / 95 + 12 * (1 + x + (x ** 3) / 28) ** 4 + (x ** 6) / 83
+    elif 55 <= x < 147:
+        return 54 * math.cos(x) ** 4 + x / 57
+    elif 147 <= x < 173:
+        return 53 * x + 92 * ((x ** 2) / 39) ** 3
+    else:
+        return (19 * x) ** 7 - 98 * (x ** 4)
+
+
+def main2(n, z, b, p):
+    sum1, sum2 = 0, 0
+    res = 1
+    for i in range(1, n):
+        sum1 += ((9 * (z ** 2) + z) ** 3 - (i + 29 + z ** 3) ** 7)
+
+    for k in range(1, n):
+        for c in range(1, b):
+            sum2 += ((k ** 5) / 79 + 15 * (89 + c ** 2 + p) ** 4)
+    res *= sum2
+
+    return '{:.2e}'.format(sum1 + res)
+
+
+# print(main2(3, -0.57, 8, 0.38))
+# print(main2(4, -0.59, 7, -0.99))
+
+def main3(n):
+    if n == 0:
+        return 0.52
+    elif n == 1:
+        return 0.06
+    elif n >= 2:
+        return (main3(n - 2) ** 2) / 37 + 1 + main3(n - 1)
+
+
+# print(main3(5))
+
+def main(*lst):
+    def vector(a):
+        return (a ** 2 + 1 + 90 * a ** 3) ** 7
+    x = lst
+    res = 0
+    for i in range(len(x)):
+        res += vector(x[i])
+
+    print(f'{86 * res:.2e}')
+
+
+main([0.51, -0.68, -0.17, 0.14])
+
+
+def f(*lst):
+    def fun(a, b):
+        return (22 * a + 1 + 13 * b ** 2) ** 7
+
+    z, x = lst
+    res = 0
+    for i in range(len(x)):
+        res += fun(z[i // 3], x[i])
+
+    print(f'{73 * res:.2e}')
+
+
+f([-0.15, 0.43], [-0.04, -0.81])
+f([0.79, -0.12], [-0.5, -0.53])
